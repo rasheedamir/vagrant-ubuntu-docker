e0d394b71a6a1c33a14d7621c648b78f00f6a918
Rasheed Amir <rasheed.amir@live.com>
2015-04-23 07:37:41 +0200

    initial release

---
+++ definitions/ubuntu-14.04-amd64-vbox/docker.sh
@@ -0,0 +1,13 @@
+# Add the Docker repository key to your local keychain
+sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+# Add the Docker repository to your apt sources list.
+sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+
+# Update your sources
+apt-get -y update
+
+# Install docker
+apt-get -y install lxc-docker
+
+sed -i "s~^.*/usr/bin/docker -d~  /usr/bin/docker -d -r -H 'unix:///var/run/docker.sock' -H 'tcp://0.0.0.0:4243' -api-enable-cors~" /etc/init/docker.conf
+service docker restart
